fc519f0aa932d61880ad22ac1cf1d8efa709c586
Esteban <estebanespg1@gmail.com>
2025-01-21 15:05:13 -0500

    font fix

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,15 @@
 import { Sora } from "next/font/google";
 import "./globals.css";
 
-const soraSans = Sora({
-  variable: "--font-sora-sans",
-  subsets: ["latin"],
-});
+const soraSans = Sora({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Notarium",
-  description: "Notarium web app",
-};
+export const metadata = { title: "Notarium", description: "Notarium web app" };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${soraSans.variable} antialiased`}
+        className={`${soraSans.className} antialiased`}
       >
         {children}
       </body>
