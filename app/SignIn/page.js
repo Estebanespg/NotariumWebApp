aa8665326cc729a8f60e5d0579e4a9eb3bad64ad
Esteban <estebanespg1@gmail.com>
2025-01-22 19:35:14 -0500

    rendering image

--- app/SignIn/page.js
+++ app/SignIn/page.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
+import icon from "../../public/ic_notarium_light_white.png";
 
 export default function SignIn() {
   return (
     <>
+      <Image src={icon}  width="512" height="512" alt=""/>
       <p className="text-2xl">Sign In</p>
       <Link href="/SignUp">Go to Sign Up</Link>
     </>
