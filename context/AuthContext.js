50a794c69e9c72ad6d0cff07a0a246209846273f
Esteban <estebanespg1@gmail.com>
2025-01-24 02:39:18 -0500

    trying deploy

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -4,21 +4,46 @@ import { createContext, useEffect, useReducer } from 'react';
 import AuthReducer from './AuthReducer';
 
 const INITIAL_STATE = {
-    currentUser: JSON.parse(localStorage.getItem("user")) || null,
+  currentUser: typeof window !== "undefined" ? JSON.parse(localStorage.getItem("user")) || null : null,
 };
+
+// const INITIAL_STATE = {
+//   currentUser: typeof window !== "undefined" ? JSON.parse(localStorage.getItem("user")) || null : null,
+// };
+
+// const INITIAL_STATE = {
+//     currentUser: JSON.parse(localStorage.getItem("user")) || null,
+// };
 
 export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
-    useEffect(() => {
-        localStorage.setItem("user", JSON.stringify(state.currentUser));
-    }, [state.currentUser]);
+  // Sincronizar con localStorage para obtener el usuario en el cliente
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const user = JSON.parse(localStorage.getItem("user"));
+      if (user) {
+        dispatch({ type: "LOGIN", payload: user });
+      }
+    }
+  }, []); // Esto solo se ejecuta una vez, al montar el componente
 
-    return (
-        <AuthContext.Provider value={{ currentUser: state.currentUser, dispatch }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  // Guardar los cambios de usuario en localStorage
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("user", JSON.stringify(state.currentUser));
+    }
+  }, [state.currentUser]);
+
+  // useEffect(() => {
+  //   localStorage.setItem("user", JSON.stringify(state.currentUser));
+  // }, [state.currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser: state.currentUser, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
