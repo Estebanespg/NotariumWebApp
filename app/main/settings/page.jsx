84a68f3af5e92c4f0c4e68223e8f7201ef5cf2bd
Esteban <estebanespg1@gmail.com>
2025-01-27 20:44:13 -0500

    settings view improvements

--- app/main/settings/page.jsx
+++ app/main/settings/page.jsx
@@ -8,15 +8,15 @@ export default function Settings() {
       </div>
       <div className="flex flex-col h-full justify-center gap-10">
         <div className="flex flex-col items-center gap-1">
-          <p className="text-lg font-semibold">¿Desea generar un reporte académico?</p>
-          <button className="w-72 h-12 bg-[#6440a5] hover:bg-[#7d56c7] font-medium border-none rounded-lg flex items-center justify-center mb-5">
+          <p className="text-lg font-medium">¿Desea generar un reporte académico?</p>
+          <button className="w-72 h-12 bg-[#6440a5] hover:bg-[#7d56c7] font-normal border-none rounded-lg flex items-center justify-center mb-5">
             <FaFilePdf />
             <p className="color-white ml-2 ">Descargar</p>
           </button>
         </div>
         <div className="flex flex-col items-center gap-1">
-          <p className="text-lg font-semibold">¿Desea eliminar su cuenta?</p>
-          <button className="w-72 h-12 bg-[#e71b1b] hover:bg-[#f93a3a] font-medium border-none rounded-lg flex items-center justify-center mb-5">
+          <p className="text-lg font-medium">¿Desea eliminar su cuenta?</p>
+          <button className="w-72 h-12 bg-[#e71b1b] hover:bg-[#f93a3a] font-normal border-none rounded-lg flex items-center justify-center mb-5">
             <p className="color-white ml-2 ">Eliminar Cuenta</p>
           </button>
         </div>
