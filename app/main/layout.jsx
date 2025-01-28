95b672fbcba57f6dab27922ec360bf0352ba6103
Esteban <estebanespg1@gmail.com>
2025-01-27 20:42:24 -0500

    faster navigation with Link component

--- app/main/layout.jsx
+++ app/main/layout.jsx
@@ -9,6 +9,7 @@ import { auth } from "@/firebase";
 import { useRouter } from "next/navigation";
 import { useContext } from "react";
 import { AuthContext } from "@/context/AuthContext";
+import Link from "next/link";
 
 export default function MainLayout({ children }) {
   const currentRoute = usePathname();
@@ -39,22 +40,22 @@ export default function MainLayout({ children }) {
 
       <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-52 h-dvh transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
         <div className="flex flex-col h-full px-3 py-4 overflow-y-auto bg-gray-800">
-          <a href="/main/students" className="flex items-center ps-2.5 mb-5">
+          <Link href="/main/students" className="flex items-center ps-2.5 mb-5">
             <Image src={icon} width="30" height="30" className="mr-2" alt="Notarium Logo" />
             <span className="self-center text-xl font-semibold whitespace-nowrap text-white">Notarium</span>
-          </a>
+          </Link>
           <ul className="space-y-3 font-medium mt-2">
             <li>
-              <a href="/main/students" className={`flex items-center p-2 ${currentRoute === '/main/students' ? 'text-white' : 'text-gray-400'} transition duration-100 hover:text-white rounded-lg hover:bg-gray-700 group`}>
+              <Link href="/main/students" className={`flex items-center p-2 ${currentRoute === '/main/students' ? 'text-white' : 'text-gray-400'} transition duration-100 hover:text-white rounded-lg hover:bg-gray-700 group`}>
                 <FaUserGraduate className={`flex-shrink-0 w-5 h-5 ${currentRoute === '/main/students' ? 'text-white' : 'text-gray-400'} transition duration-100 group-hover:text-white`} aria-hidden="true" fill="currentColor" />
                 <span className="flex-1 ms-3 whitespace-nowrap">Estudiantes</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/main/settings" className={`flex items-center p-2 ${currentRoute === '/main/settings' ? 'text-white' : 'text-gray-400'} transition duration-100 hover:text-white rounded-lg hover:bg-gray-700 group`}>
+              <Link href="/main/settings" className={`flex items-center p-2 ${currentRoute === '/main/settings' ? 'text-white' : 'text-gray-400'} transition duration-100 hover:text-white rounded-lg hover:bg-gray-700 group`}>
                 <FaGear className={`flex-shrink-0 w-5 h-5 ${currentRoute === '/main/settings' ? 'text-white' : 'text-gray-400'} transition duration-100 group-hover:text-white`} aria-hidden="true" fill="currentColor" />
                 <span className="flex-1 ms-3 whitespace-nowrap">Configuración</span>
-              </a>
+              </Link>
             </li>
           </ul>
           <ul className="pt-4 mt-auto space-y-2 font-medium border-t border-gray-700">
