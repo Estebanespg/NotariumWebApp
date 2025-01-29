960c1e2811369c5b019ad62825c666a2cce663e7
Esteban <estebanespg1@gmail.com>
2025-01-28 23:47:43 -0500

    student view

---
+++ app/main/[student]/page.jsx
@@ -0,0 +1,9 @@
+export default function Student() {
+  return (
+    <div className="flex flex-col h-full items-center">
+      <div className="flex flex-col items-center mb-2">
+        <p className="text-2xl font-semibold">Estudiante</p>
+      </div>
+    </div>
+  );
+}
