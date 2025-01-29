32ccb552993170b2803fb36db3846d55422d414f
Esteban <estebanespg1@gmail.com>
2025-01-28 23:54:59 -0500

    slug page

--- app/main/students/[student]/page.jsx
+++ app/main/students/[student]/page.jsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { useRouter } from 'next/navigation'
+
 export default function Student() {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col h-full items-center">
       <div className="flex flex-col items-center mb-2">
         <p className="text-2xl font-semibold">Estudiante</p>
+        <p>Post: {router.query.slug}</p>
       </div>
     </div>
   );
--- components/StudentCard.jsx
+++ components/StudentCard.jsx
@@ -1,14 +1,13 @@
-import Link from "next/link";
 import { FaTrashCan } from "react-icons/fa6";
 
 export default function StudentCard({data}) {
   return (
-    <Link href="/main/students/student" className="flex flex-row mt-5 bg-slate-800 hover:bg-slate-700 px-6 rounded-lg w-80 sm:w-96 min-h-20 justify-between items-center cursor-pointer">
+    <a className="flex flex-row mt-5 bg-slate-800 hover:bg-slate-700 px-6 rounded-lg w-80 sm:w-96 min-h-20 justify-between items-center cursor-pointer">
       <div>
         <p className="color-white font-normal text-lg">{data.student}</p>
         <p className="color-slate-400 font-light text-base">{Object.keys(data.subjects).length} {Object.keys(data.subjects).length === 1 ? 'Asignatura' : 'Asignaturas'}</p>
       </div>
       <button><FaTrashCan className="w-5 h-5 text-[#e71b1b]" /></button>
-    </Link>
+    </a>
   );
 }
